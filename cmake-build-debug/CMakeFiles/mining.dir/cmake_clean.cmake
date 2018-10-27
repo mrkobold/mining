5e41ac84bea4a561315643da2bc3c9b0e99063fd
KoBold <kovacsboldizsar@gmail.com>
2018-10-27 18:43:50 +0300

    Create CLion Project

---
+++ cmake-build-debug/CMakeFiles/mining.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mining.dir/main.c.obj"
+  "mining.pdb"
+  "mining.exe"
+  "mining.exe.manifest"
+  "libmining.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mining.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
